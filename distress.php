f29666fea7fcda0d0dca6b0748877d148e889c61
Sarbeng <35800995+Sarbeng@users.noreply.github.com>
2019-10-03 09:52:24 +0100

    Add files via upload
    New pages added

--- distress.php
+++ distress.php
@@ -1,75 +1,45 @@
+<?php
+$host='localhost';
+$user='root';
+$password='';
+$db = 'distress_call';
+
+$con = mysqli_connect($host,$user,$password);
+mysqli_select_db($con,$db);
+
+if (isset($_POST['submit'])){
+
+//variables for the external values to insert into query
+  $lat = 2332.343;
+  $lon = 234.4874;
+  $map_url = 'http://hope.com';
+
+  $sql = "Insert into distress(lat,lon,map_url) values('".$lat."','".$lon."','".$map_url."')";
+
+  //entering the data into the database
+  $result = mysqli_query($con,$sql);
+  if (mysqli_num_rows($result)==1){
+    echo"Data inserted Successfully";
+    exit();
+  }
+  else{
+    echo"Data not inserted";
+    exit();
+  }
+}
+
+?>
 
 <!DOCTYPE html>
-<html>
-
-<head>
-        <link rel="manifest" href="manifest.json">
-
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="application-name" content="PWA from scratch">
-<meta name="apple-mobile-web-app-title" content="PWA from scratch">
-<meta name="theme-color" content="#c2f442">
-<meta name="msapplication-navbutton-color" content="blue">
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-<meta name="msapplication-starturl" content="/index.html">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<link rel="icon" sizes="64x64" href="1.jpeg">
-<link rel="apple-touch-icon" sizes="64x64" href="1.jpeg">
-<link rel="icon" sizes="128X128" href="2.jpeg">
-<link rel="apple-touch-icon" sizes="128X128" href="2.jpeg">
-<link rel="icon" sizes="512x512" href="ucc.gif">
-<link rel="apple-touch-icon" sizes="512x512" href="aau.png">
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Distress call</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <link rel="manifest" href="manifest.json">
-
-    <script src="main.js"></script>
-</head>
-
-<body>
-    <div>
-        <!-- <input id="anime_id_input" placeholder="Anime id" />
-        <button id="ok_button" onclick="onOkButtonClickAsync()">OK</button>
-    </div>
-    <div id="main_anime">
-    </div>
-    <h1>History</h1>
-    <div id="history"> -->
-    <div class="hero flex-center">
-        <p class="">jonah</p>
-    <form method="post" action="index.php">
-        hello
-      <h1><input class="panic" value="submit" type="submit"/></h1>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="#" method="POST">
+      <input type="submit" name="submit" value="submit">
     </form>
-  </div>
-    <?php
-    function send_sms(){
-    require_once "vendor/autoload.php";
 
-    $basic  = new \Nexmo\Client\Credentials\Basic('', '');
-    $client = new \Nexmo\Client($basic);
-
-    $message = $client->message()->send([
-        'to' => '233245391083',
-        'from' => 'Nexmo',
-        'text' => 'Hello from Nexmo'
-    ]);
-    }
-
-    if($_SERVER['REQUEST_METHOD']=='POST')
-           {
-               send_sms();
-           }
-
-
-    ?>
-
-    </div>
-</body>
-
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,110 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Distress Call</title>
-  <!--adding the css-->
-  <link type="text/css" rel="stylesheet" href="css/materialize.css" />
-  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
-  <!--Adding the css for the font-->
-  <link type="text/css" rel="stylesheet" href="css/materializeicon.css"/>
-</head>
-<body>
-  <script type="text/javascript" src="/js/materialize.js"></script>
-  <script type="text/javascript" src="/js/materialize.min.js"></script>
-  <div class="container">
-    <div class="row">
-      <div class="row"></div>
-      <div class="row"></div>
-      <div class="row"></div>
-      <div class="row"></div>
-      <div class="row"></div>
-      <div class="row"></div>
-      <div class="row"></div>
-    
-        
-    
-      <div class="col s12 show-medium-and-up center">
-        
-            <button type="submit" name="action" id="panic" class="btn btn-floating pulse" onclick="panic();">
-                <h1 id="sos" class="flow-text">SOS</h1>
-            </button>
-       
-         
-        </div>
-      
-      <div class="col m4">
-        
-        </div>
-    </div>
-    
-  </div>
-  <style>
-    
-    #panic{
-      width:20rem;
-      height:20rem;
-      background:red;
-      position:relative;
-      top:30px;
-    }
-
-    #sos{
-       position:relative;
-       top:20%;
-       font-size:5rem;
-    }
-  </style>
-
-
-<p id='loc'><p>
-<script>
-  var x = document.getElementById("loc");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-//var la;
-//var lo;
-function showPosition(position) {
-export var  la =position.coords.latitude;
- export var lo= position.coords.longitude;
-  x.innerHTML = "https://maps.google.com/?q="+la + ","+ lo;
-}
-getLocation();
-//function to send google maps link
-// function coordinates (la,lo){
-//   var maps_code = "https://maps.google.com/?q="+la + ","+ lo;
-//   return maps_code;
-// }
-
-
-// var coordinates = coordinates();
-</script>
 <?php
+include_once('connection.php');
+$query ="Select *from distress order by id desc ";
+$result = mysqli_query($con,$query);
+ //?>
 
-function panic(){
-  $basic  = new \Nexmo\Client\Credentials\Basic('', '');
-  $client = new \Nexmo\Client($basic);
-  
-  $message = $client->message()->send([
-      'to' => '233246971090',
-      'from' => 'Nexmo',
-      'text' => "Shiiiiiiiiiiiiiiiiiiiitttttttt  https://maps.google.com/?q=$la!,$lo!"
-      
-      
-  ]);
-  
-  echo "message sent";
-  
-}
-//panic();
-?>
- 
-</body>
-</html>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Fetch Data from Database</title>
+   </head>
+   <body>
+     <table algin="center" border="1px" style="width:600px; line-height:40px;">
+       <tr><th colspan="4">Student Record</th></tr>
+       <tr>
+
+         <th>ID</th>
+         <th>Latitude</th>
+         <th>Longitude</th>
+         <th>Map_URL</th>
+       </tr>
+       <?php
+          while($rows=mysqli_fetch_assoc($result)){
+        ?>
+            <tr>
+              <td><?php echo $rows['ID']; ?></td>
+              <td><?php echo $rows['lat']; ?></td>
+              <td><?php echo  $rows['lon']; ?></td>
+              <?php $url=$rows['map_url'];
+               echo "<td> <a href='" . $url . "'>" . $url . "</a> </td>" ?>
+            <?php
+            }
+             ?>
+
+
+     </table>
+   </body>
+ </html>
